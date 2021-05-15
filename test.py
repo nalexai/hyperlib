5def2307140dadf2d5bb701288fa6d647e32cb6c
sourface <nathan_jf@hotmail.co.uk>
2021-05-15 12:32:02 +0100

    Added test file and refining library structure

---
+++ test.py
@@ -0,0 +1,14 @@
+from manifold import poincare
+from utils import math
+
+def math_functions():
+    raise NotImplementedError
+
+def poincare_functions():
+    raise NotImplementedError
+
+def create_layer():
+    raise NotImplementedError
+    
+def create_optimizer():
+    raise NotImplementedError
