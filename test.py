1c0ea866745bb7a6a28f12a7c6b9af4cf9abe834
sourface94 <nathan_jf@hotmail.co.uk>
2021-05-15 16:30:00 +0100

    Added additional test code to test.py

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
+import tensorflow as tf
 from manifold import poincare
 from utils import math
+from nn.layers import lin_hyp
+from nn.optimizers import rsgd
 
-def math_functions():
-    raise NotImplementedError
+def math_functions(x=tf.constant([1.0,2.0,3.0])):
+    return math.cosh(x)
 
 def poincare_functions():
-    raise NotImplementedError
+    manifold = poincare.PoincareTF()
+    return manifold
 
 def create_layer():
-    raise NotImplementedError
-    
+    hyp_layer = lin_hyp.LinearHyperbolic()
+    return hyp_layer
+
 def create_optimizer():
-    raise NotImplementedError
+    opt = rsgd.RSGD()
+    return opt
