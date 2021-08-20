55829b7ee541de9a4c69adbfb5b58a148f4afcb5
meiji163 <mysatellite99@gmail.com>
2021-08-19 18:03:13 -0700

    fix mutiprec

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import find_packages
-from distutils.core import setup, Extension
-from pybind11.setup_helpers import Pybind11Extension 
 import pybind11
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
 from glob import glob
 import sys
 
 __version__ = "0.0.3"
 
 ext_modules = [
-        Pybind11Extension("_embedding",
+        Extension("_embedding",
             sorted(glob("hyperlib/embedding/src/*.cc")),
             include_dirs = ["hyperlib/embedding/include", pybind11.get_include()],
-            cxx_std = "11"
-        )
+            language = "c++",
+            extra_compile_args = ["-std=c++11"]
+        ),
 ]
 
 setup(
@@ -20,7 +20,7 @@ setup(
     packages = find_packages(),
     setup_requires = ["pip>=21"],
     install_requires = ["numpy>=1.19.3", 
-						"tensorflow>=2.0.0",
+						#"tensorflow>=2.0.0",
 						"scipy", 
                         "mpmath"],
     version=__version__,
@@ -28,5 +28,6 @@ setup(
     author_email="info@nalexai.com",
     description="A hyperbolic deep learning library",
     ext_modules=ext_modules,
-    license_files = "LICENSE"
+    license_files = "LICENSE",
+    zip_safe = False
 )
