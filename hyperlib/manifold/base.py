fc1c4a8d4f46d3dadaf44442de473847fc9c4ae5
aaj-fullfact <$Ferrari4>
2021-08-26 21:29:19 +0100

    removed mobius functions from the base manifold class.

--- hyperlib/manifold/base.py
+++ hyperlib/manifold/base.py
@@ -11,29 +11,13 @@ class Manifold(object):
         self.min_norm = 1e-15
         self.eps = {tf.float32: 4e-3, tf.float64: 1e-5}
 
-    def mobius_matvec(self, m, x, c):
-        """
-        Generalization for matrix-vector multiplication to hyperbolic space defined as
-        math::
-            M \otimes_c x = (1/\sqrt{c}) \tanh\left(
-                \frac{\|Mx\|_2}{\|x\|_2}\tanh^{-1}(\sqrt{c}\|x\|_2)
-            \right)\frac{Mx}{\|Mx\|_2}
-        Args:
-            m : Tensor for multiplication
-            x : Tensor point on poincare ball
-            c : Tensor of size 1 representing the hyperbolic curvature.
-        Returns
-            Mobius matvec result
-        """
-
-        raise NotImplementedError
 
     def expmap(self, u, p, c):
         raise NotImplementedError
 
     def expmap0(self, u, c):
         """
-        Hyperbolic exponential map at zero in the Poincare ball model.
+        Hyperbolic exponential map at zero.
           Args:
             u: tensor of size B x dimension representing tangent vectors.
             c: tensor of size 1 representing the hyperbolic curvature.
@@ -44,7 +28,7 @@ class Manifold(object):
 
     def logmap0(self, p, c):
         """
-        Hyperbolic logarithmic map at zero in the Poincare ball model.
+        Hyperbolic logarithmic map at zero.
         Args:
           p: tensor of size B x dimension representing hyperbolic points.
           c: tensor of size 1 representing the hyperbolic curvature.
@@ -69,18 +53,6 @@ class Manifold(object):
             https://arxiv.org/abs/1805.09112
         """
 
-        raise NotImplementedError
-
-    def mobius_add(self, x, y, c):
-        """Element-wise Mobius addition.
-      Args:
-        x: Tensor of size B x dimension representing hyperbolic points.
-        y: Tensor of size B x dimension representing hyperbolic points.
-        c: Tensor of size 1 representing the absolute hyperbolic curvature.
-      Returns:
-        Tensor of shape B x dimension representing the element-wise Mobius addition
-        of x and y.
-      """
         raise NotImplementedError
 
     def hyp_act(self, act, c_in, c_out):
