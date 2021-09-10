a66313aa91c737ee68eacac0ba86523d302901fc
aaj-fullfact <$Ferrari4>
2021-09-10 20:20:59 +0100

    updated layers to be compbatible with new implementation of manifolds. Added attention layer and accompanying test

--- hyperlib/nn/layers/lin_hyp.py
+++ hyperlib/nn/layers/lin_hyp.py
@@ -38,14 +38,14 @@ class LinearHyperbolic(keras.layers.Layer):
         """
         # TODO: remove casting and instead recommend setting default tfd values to float64
         inputs = tf.cast(inputs, tf.float64)
-        mv = self.manifold.mobius_matvec(self.kernel, inputs)
-        res = self.manifold.proj(mv)
+        mv = self.manifold.mobius_matvec(self.kernel, inputs, self.c)
+        res = self.manifold.proj(mv, c=self.c)
 
         if self.use_bias:
-            hyp_bias = self.manifold.expmap0(self.bias)
-            hyp_bias = self.manifold.proj(hyp_bias)
-            res = self.manifold.mobius_add(res, hyp_bias)
-            res = self.manifold.proj(res)
+            hyp_bias = self.manifold.expmap0(self.bias, c=self.c)
+            hyp_bias = self.manifold.proj(hyp_bias, c=self.c)
+            res = self.manifold.mobius_add(res, hyp_bias, c=self.c)
+            res = self.manifold.proj(res, c=self.c)
 
         return self.activation(res)
 
--- tests/test_poincare.py
+++ tests/test_poincare.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from hyperlib.manifold import poincare
 from hyperlib.utils import functional as F
-from hyperlib.nn.layers import lin_hyp
+from hyperlib.nn.layers import lin_hyp, dense_attention
 from hyperlib.nn.optimizers import rsgd
 import pytest
 
@@ -17,6 +17,7 @@ class TestClass:
         self.test_tensor_shape_2_1a = tf.constant([[1.0], [2.0]], dtype=tf.float64)
         self.test_tensor_shape_2_1b = tf.constant([[0.5], [0.5]], dtype=tf.float64)
         self.poincare_manifold = poincare.Poincare()
+        self.curvature_tensor = tf.Variable([1], dtype="float64")
 
     def test_math_functions(self, x=tf.constant([1.0, 2.0, 3.0])):
         return F.cosh(x)
@@ -24,27 +25,28 @@ class TestClass:
     def test_mobius_matvec_(self):
         result = self.poincare_manifold.mobius_matvec(
             self.test_tensor_shape_2_2_a,
-            self.test_tensor_shape_2_2_b
+            self.test_tensor_shape_2_2_b, c=self.curvature_tensor
         )
         with pytest.raises(tf.errors.InvalidArgumentError):
             self.poincare_manifold.mobius_matvec(
                 self.test_tensor_shape_2_2_a,
-                self.test_tensor_shape_2_1a
+                self.test_tensor_shape_2_1a, c=self.curvature_tensor
             )
 
     def test_expmap0(self):
+        c = tf.Variable([1], dtype="float64")
         result = self.poincare_manifold.expmap0(
-            self.test_tensor_shape_2_2_a 
+            self.test_tensor_shape_2_2_a, c=self.curvature_tensor
         )
 
     @pytest.mark.skip(reason="working on a test for this")
     def test_logmap0(self):
         result = self.poincare_manifold.logmap0(
-            self.test_tensor_shape_2_2_b, self.curvature_tensor
+            self.test_tensor_shape_2_2_b, c=self.curvature_tensor
         )
 
     def test_proj(self):
-        result = self.poincare_manifold.proj(self.test_tensor_shape_2_2_a)
+        result = self.poincare_manifold.proj(self.test_tensor_shape_2_2_a, c=self.curvature_tensor)
 
     def test_poincare_functions(self):
         manifold = poincare.Poincare()
@@ -57,6 +59,17 @@ class TestClass:
         )
         assert hyp_layer.units == units
         assert hyp_layer.manifold == self.poincare_manifold
+
+    def test_attention_layer(self):
+        sample_hidden = tf.Variable(tf.random.uniform([10, 1], 0, 100, dtype=tf.float64, seed=0))
+        sample_output = tf.Variable(tf.random.uniform([10, 1], 0, 100, dtype=tf.float64, seed=0))
+
+        attention_layer = dense_attention.HypLuongAttention(manifold=self.poincare_manifold, c=1, use_scale=False,
+                                                            hyperbolic_input=False)
+        query_value_attention_seq = attention_layer([sample_hidden, sample_output])
+
+        assert query_value_attention_seq.shape == sample_output.shape
+        assert attention_layer.manifold == self.poincare_manifold
 
     def test_layer_training(self, units=32):
         x_input = tf.zeros([units, 1])
