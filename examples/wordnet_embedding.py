7fac1015413f5b48da96736d2caf6e1f18738e2e
sourface94 <nathan_jf@hotmail.co.uk>
2022-06-02 20:59:54 +0100

    feat: Implement model class

--- examples/wordnet_embedding.py
+++ examples/wordnet_embedding.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 
 from hyperlib.manifold.lorentz import Lorentz
 from hyperlib.manifold.poincare import Poincare
-from hyperlib.loss.contrastive_loss import contrastive_loss
+from hyperlib.models.pehr import HierarchicalEmbeddings
 
 
 def load_wordnet_data(file, negative_samples=10):
@@ -39,32 +39,6 @@ def load_wordnet_data(file, negative_samples=10):
     noun_closure["neg_pairs"] = noun_closure["id1"].apply(lambda x: neg_samples[x])
     return noun_closure, unique_nouns
 
-def train(model, train_data, **kwargs):
-    epochs = kwargs.get("epochs", 10)
-    c = kwargs.get("c", 1.0)
-    lr = kwargs.get("lr", 1e-2)
-    clip_value = kwargs.get("clip_value", 0.9)
-    momentum = kwargs.get("momentum", 0)
-    M = Poincare()
-
-    sgd = keras.optimizers.SGD(learning_rate=lr, momentum=momentum)
-
-    for epoch in range(epochs):
-        print("Epoch %d" % (epoch,))
-        for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
-            with tf.GradientTape() as tape:
-                pos_embs = model(x_batch_train, training=True)
-                neg_embs = model(y_batch_train)
-                loss_value = contrastive_loss(
-                        pos_embs, neg_embs, M, c=c, clip_value=clip_value)
-
-            grads = tape.gradient(loss_value, model.trainable_weights)
-            sgd.apply_gradients(zip(grads, model.trainable_weights))
-
-            if step % 100 == 0:
-                print("Training loss (for one batch) at step %d: %.4f"
-                    % (step, float(loss_value)))
-
 
 # Make training dataset
 noun_closure, unique_nouns = load_wordnet_data("data/mammal_closure.csv")
@@ -75,28 +49,10 @@ train_dataset = tf.data.Dataset.from_tensor_slices(
         (noun_closure_dataset, noun_closure["neg_pairs"].tolist()))
 train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
 
+# Create model
+model = HierarchicalEmbeddings(vocab=unique_nouns, embedding_dim=5)
+sgd = keras.optimizers.SGD(learning_rate=1e-2, momentum=0.9)
 
-# define embedding layer
-embedding_dim = 5
-initializer=keras.initializers.RandomUniform(minval=-0.001, maxval=0.001, seed=None)
-string_lookup_layer = keras.layers.StringLookup(vocabulary=unique_nouns)
-
-emb_layer = keras.layers.Embedding(
-    len(unique_nouns)+1,
-    embedding_dim,
-    input_length=2,
-    embeddings_initializer=initializer,
-    name="embeddings",
-)
-
-model = keras.Sequential([string_lookup_layer, emb_layer])
-
-train(
-    model,
-    train_dataset,
-    c=1.0,
-    epochs=10,
-    clip_value=0.9,
-    lr=1e-2,
-    momentum=0.9,
-)
+# Run custom training loop
+model.fit(train_dataset, sgd, epochs=5)
+print(model.get_embeddings())
